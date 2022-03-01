da162fa250797db160bdcc6e87d3c8ef98fb4778
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-03-01 10:34:22 +0530

    Second Commit

--- LockedMe Prototype/src/lockedme_prototype/LockedMe_Prototype.java
+++ LockedMe Prototype/src/lockedme_prototype/LockedMe_Prototype.java
@@ -25,14 +25,14 @@ public class LockedMe_Prototype {
 	}
 	
 	//DECLARATION OF MENUS
-	private  static final String MainMenu =
+	private static final String MainMenu =
             "\n\tMain Menu "
             + "\nSelect your option: \n"+
                     "1) List files in directory\n"+
                     "2) Add / Delete / Search\n"+
                     "3) Exit";
 
-	private  static final String SubMenu =
+	private static final String SubMenu =
             "   \nSelect any of the following: \n"+
                     "a) Add file\n"+
                     "b) Delete file\n"+
@@ -41,15 +41,15 @@ public class LockedMe_Prototype {
 	
 	
     //OPTIONS FOR PRIMARY MENU
-	void showPrimaryMenu() {
+	void MainMenu() {
         System.out.println(MainMenu);
         try{
             Scanner sc = new Scanner(System.in);
             int opt = sc.nextInt();
             switch (opt){
                 case 1 : {
-                    showFiles();
-                    showPrimaryMenu();
+                    ViewFiles();
+                    MainMenu();
                 }
                 case 2 : {
                 	SubMenu();
@@ -58,18 +58,18 @@ public class LockedMe_Prototype {
                     System.out.println("Thanks for using LockedMe.com, Have a great day!!!");
                     System.exit(0);
                 }
-                default: showPrimaryMenu();
+                default: MainMenu();
             }
         }
         catch (Exception e){
             System.out.println("Please enter 1 / 2 / 3");
-            showPrimaryMenu();
+            MainMenu();
         }
     }
     
 	
 	
-    //OPTIONS OF SUB-MENU
+    //OPTIONS FOR SUB-MENU
 	private void SubMenu() {
 		System.out.println(SubMenu);
         try{
@@ -98,7 +98,7 @@ public class LockedMe_Prototype {
                 }
                 case 'd' : {
                     System.out.println("Exit to Main Menu");
-                    showPrimaryMenu();
+                    MainMenu();
                     break;
                 }
                 default : System.out.println("Invalid selection, Please select a / b / c / d");
@@ -122,7 +122,7 @@ public class LockedMe_Prototype {
                 return;
             }
         }
-        System.out.println("File Not Found");
+        System.out.println("No such file exists");    //IN CASE OF WRONG FILE NAME
     }
 
 ////////////////////////////////////FOR DELETING FILES////////////////////////////////////
@@ -135,7 +135,7 @@ public class LockedMe_Prototype {
                 return;
             }
         }
-        System.out.println("Deletion Failed. FILE NOT FOUND...");
+        System.out.println("Deletion Failed. FILE NOT FOUND...");   //IN CASE OF WRONG FILE NAME
     }
 
 //////////////////////////////////FOR ADDING FILES///////////////////////////////////////
@@ -153,13 +153,13 @@ public class LockedMe_Prototype {
     }
 
 ////////////////////////////////////FOR VIEWING FILES/////////////////////////////////////
-	private void showFiles() {
-		if (foldername.list().length==0)
+	private void ViewFiles() {
+		if (foldername.list().length==0)  //EMPTY DIRECTORY
             System.out.println("The folder is empty");
-        else {
+        else {                            // (!EMPTY) DIRECTORY
             String[] list = foldername.list();
             System.out.println("The files in "+ foldername +" are :");
-            Arrays.sort(list);
+            Arrays.sort(list);            //SORTING THE ARRAY AS ASCENDING
             for (String str:list) {
                 System.out.println(str);
             }
@@ -172,7 +172,7 @@ public class LockedMe_Prototype {
 		System.out.println("\t\t\t\t\t\tAPPLICATION NAME : LockedMe.com");
 		System.out.println("\n\t\t\t\t\t\tDEVELOPER : Goutham Gummadipudi");
 		LockedMe_Prototype Demo = new LockedMe_Prototype();
-        Demo.showPrimaryMenu();
+        Demo.MainMenu();
 
 	}
 
